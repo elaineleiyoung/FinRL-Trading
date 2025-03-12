389eea92bbf9fc9a2a6895010a2853ec58140b03
elaineleiyoung <elaine.leiyoung42@gmail.com>
2025-03-12 00:43:43 -0700

    updated requirements, imports, and syntax

--- DRL-for-Trading/model/models.py
+++ DRL-for-Trading/model/models.py
@@ -4,18 +4,11 @@ import numpy as np
 import time
 import gym
 
-# RL models from stable-baselines
-from stable_baselines import GAIL, SAC
-from stable_baselines import ACER
-from stable_baselines import PPO2
-from stable_baselines import A2C
-from stable_baselines import DDPG
-from stable_baselines import TD3
+# Stable Baselines3 RL models
+from stable_baselines3 import A2C, PPO, DDPG, TD3
+from stable_baselines3.common.noise import OrnsteinUhlenbeckActionNoise
+from stable_baselines3.common.vec_env import DummyVecEnv
 
-from stable_baselines.ddpg.policies import DDPGPolicy
-from stable_baselines.common.policies import MlpPolicy, MlpLstmPolicy, MlpLnLstmPolicy
-from stable_baselines.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise, AdaptiveParamNoiseSpec
-from stable_baselines.common.vec_env import DummyVecEnv
 from preprocessing.preprocessors import *
 from config import config
 
@@ -37,15 +30,15 @@ def train_A2C(env_train, model_name, timesteps=25000):
     print('Training time (A2C): ', (end - start) / 60, ' minutes')
     return model
 
-def train_ACER(env_train, model_name, timesteps=25000):
-    start = time.time()
-    model = ACER('MlpPolicy', env_train, verbose=0)
-    model.learn(total_timesteps=timesteps)
-    end = time.time()
+# def train_ACER(env_train, model_name, timesteps=25000):
+#     start = time.time()
+#     model = ACER('MlpPolicy', env_train, verbose=0)
+#     model.learn(total_timesteps=timesteps)
+#     end = time.time()
 
-    model.save(f"{config.TRAINED_MODEL_DIR}/{model_name}")
-    print('Training time (A2C): ', (end - start) / 60, ' minutes')
-    return model
+#     model.save(f"{config.TRAINED_MODEL_DIR}/{model_name}")
+#     print('Training time (A2C): ', (end - start) / 60, ' minutes')
+#     return model
 
 
 def train_DDPG(env_train, model_name, timesteps=10000):
@@ -57,7 +50,7 @@ def train_DDPG(env_train, model_name, timesteps=10000):
     action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=float(0.5) * np.ones(n_actions))
 
     start = time.time()
-    model = DDPG('MlpPolicy', env_train, param_noise=param_noise, action_noise=action_noise)
+    model = DDPG('MlpPolicy', env_train, action_noise=action_noise, verbose=0)
     model.learn(total_timesteps=timesteps)
     end = time.time()
 
@@ -69,8 +62,7 @@ def train_PPO(env_train, model_name, timesteps=50000):
     """PPO model"""
 
     start = time.time()
-    model = PPO2('MlpPolicy', env_train, ent_coef = 0.005, nminibatches = 8)
-    #model = PPO2('MlpPolicy', env_train, ent_coef = 0.005)
+    model = PPO('MlpPolicy', env_train, ent_coef=0.005, batch_size=64, verbose=0)
 
     model.learn(total_timesteps=timesteps)
     end = time.time()
@@ -79,24 +71,24 @@ def train_PPO(env_train, model_name, timesteps=50000):
     print('Training time (PPO): ', (end - start) / 60, ' minutes')
     return model
 
-def train_GAIL(env_train, model_name, timesteps=1000):
-    """GAIL Model"""
-    #from stable_baselines.gail import ExportDataset, generate_expert_traj
-    start = time.time()
-    # generate expert trajectories
-    model = SAC('MLpPolicy', env_train, verbose=1)
-    generate_expert_traj(model, 'expert_model_gail', n_timesteps=100, n_episodes=10)
+# def train_GAIL(env_train, model_name, timesteps=1000):
+#     """GAIL Model"""
+#     #from stable_baselines.gail import ExportDataset, generate_expert_traj
+#     start = time.time()
+#     # generate expert trajectories
+#     model = SAC('MLpPolicy', env_train, verbose=1)
+#     generate_expert_traj(model, 'expert_model_gail', n_timesteps=100, n_episodes=10)
 
-    # Load dataset
-    dataset = ExpertDataset(expert_path='expert_model_gail.npz', traj_limitation=10, verbose=1)
-    model = GAIL('MLpPolicy', env_train, dataset, verbose=1)
+#     # Load dataset
+#     dataset = ExpertDataset(expert_path='expert_model_gail.npz', traj_limitation=10, verbose=1)
+#     model = GAIL('MLpPolicy', env_train, dataset, verbose=1)
 
-    model.learn(total_timesteps=1000)
-    end = time.time()
+#     model.learn(total_timesteps=1000)
+#     end = time.time()
 
-    model.save(f"{config.TRAINED_MODEL_DIR}/{model_name}")
-    print('Training time (PPO): ', (end - start) / 60, ' minutes')
-    return model
+#     model.save(f"{config.TRAINED_MODEL_DIR}/{model_name}")
+#     print('Training time (PPO): ', (end - start) / 60, ' minutes')
+#     return model
 
 
 def DRL_prediction(df,
@@ -124,12 +116,13 @@ def DRL_prediction(df,
         action, _states = model.predict(obs_trade)
         obs_trade, rewards, dones, info = env_trade.step(action)
         if i == (len(trade_data.index.unique()) - 2):
-            # print(env_test.render())
-            last_state = env_trade.render()
-
-    df_last_state = pd.DataFrame({'last_state': last_state})
-    df_last_state.to_csv('results/last_state_{}_{}.csv'.format(name, i), index=False)
+            # last_state = env_trade.render()
+            last_state = env_trade.envs[0].render()
+    pd.DataFrame({"last_state": last_state}).to_csv(
+        f"results/last_state_{name}_{i}.csv", index=False
+    )
     return last_state
+
 
 
 def DRL_validation(model, test_data, test_env, test_obs) -> None:
